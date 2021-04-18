5803bd78cb4f3b3f6c7a07777cdf50312d2776df
FernanEd <fernandoed.bm@gmail.com>
2021-04-17 22:31:31 -0500

    added view engine

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import path from "path";
 import morgan from "morgan";
 import cors from "cors";
 
 const app = express();
 
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.render("../views/layout.pug");
@@ -18,7 +22,10 @@ app.get("/", (req, res) => {
 import config from "./utils/config";
 
 export default async () => {
-  await mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+  await mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   app.listen(config.PORT, () => {
     console.log(`server started in port ${config.PORT}`);
   });
