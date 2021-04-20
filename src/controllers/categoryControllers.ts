00e6e99869ff09314e900f0551f16fd9645e5032
FernanEd <fernandoed.bm@gmail.com>
2021-04-19 22:13:33 -0500

    added some categories

--- src/controllers/categoryControllers.ts
+++ src/controllers/categoryControllers.ts
@@ -9,6 +9,7 @@ export const getAllCategories: Handler = async (req, res) => {
       categories: docs,
     });
   } catch (e) {
+    console.log(e);
     res.status(400).end();
   }
 };
@@ -18,5 +19,21 @@ export const getAddCategory: Handler = async (req, res) => {
 };
 
 export const postAddCategory: Handler = async (req, res) => {
-  res.send("lol");
+  const { name, description } = req.body;
+  try {
+    if (name && description) {
+      const created = await Category.create({ name, description });
+      console.log(created);
+      res.redirect("/categories");
+    } else {
+      res.render("category_add", {
+        title: "Add a new category",
+        previous_form: { name, description },
+        validation_error: "Please fill up the fields",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
 };
--- src/models/category.ts
+++ src/models/category.ts
@@ -3,7 +3,10 @@ import { Schema, model } from "mongoose";
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  url: { type: String, required: true },
+});
+
+CategorySchema.virtual("url").get(function (this: any) {
+  return `/categories/${this!._id}`;
 });
 
 export default model("Category", CategorySchema);
