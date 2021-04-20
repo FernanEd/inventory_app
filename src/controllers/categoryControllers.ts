20876217e35bbc382e91ca5b60f351d906901b39
FernanEd <fernandoed.bm@gmail.com>
2021-04-19 23:46:58 -0500

    added edit category

--- src/controllers/categoryControllers.ts
+++ src/controllers/categoryControllers.ts
@@ -14,6 +14,20 @@ export const getAllCategories: Handler = async (req, res) => {
   }
 };
 
+export const getOneCategory: Handler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const one = await Category.findById(id);
+    res.status(200).render("category_detail", {
+      title: one.name,
+      category: one,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
+
 export const getAddCategory: Handler = async (req, res) => {
   res.render("category_add", { title: "Add a new category" });
 };
@@ -23,11 +37,47 @@ export const postAddCategory: Handler = async (req, res) => {
   try {
     if (name && description) {
       const created = await Category.create({ name, description });
-      console.log(created);
       res.redirect("/categories");
     } else {
       res.render("category_add", {
         title: "Add a new category",
+        previous_form: { name, description },
+        validation_error: "Please fill up the fields",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
+
+export const getEditCategory: Handler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { name, description } = await Category.findById(id);
+    res.status(200).render("category_edit", {
+      title: "Edit category",
+      previous_form: { name, description },
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
+
+export const postEditCategory: Handler = async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  try {
+    if (name && description) {
+      const edited = await Category.findByIdAndUpdate(id, {
+        name,
+        description,
+      });
+      res.redirect("/categories");
+    } else {
+      res.render("category_edit", {
+        title: "Edit category",
         previous_form: { name, description },
         validation_error: "Please fill up the fields",
       });
--- src/routers/categoryRouter.ts
+++ src/routers/categoryRouter.ts
@@ -5,8 +5,14 @@ const router = Router();
 
 router.get("/", controller.getAllCategories);
 
+router.get("/:id", controller.getOneCategory);
+
 router.get("/add", controller.getAddCategory);
 
 router.post("/add", controller.postAddCategory);
+
+router.get("/edit/:id", controller.getEditCategory);
+
+router.post("/edit/:id", controller.postEditCategory);
 
 export default router;
